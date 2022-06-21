16b151f14f189cfd4d2c77aa1839650899aafbca
Flávio A. da Maia Jr <junior@bmasistemas.com.br>
2022-06-20 23:35:33 -0300

    Class 01 - Initial project setup.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { gql, useQuery } from "@apollo/client";
+import { client } from "./lib/apollo";
+
+function App() {
+  return <h1 className="text-5xl font-bold">Hello World!</h1>;
+}
+
+export default App;
